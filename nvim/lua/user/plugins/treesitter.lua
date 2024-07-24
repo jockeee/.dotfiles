abd5b73886a3888bbc7c41c3243f9dcc6e5e099a
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2024-07-24 03:38:37 +0200

    nvim

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -16,6 +16,15 @@ return {
     auto_install = true, -- autoinstall languages that are not installed
     highlight = { enable = true },
     indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>', -- set to `false` to disable one of the mappings
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
+      },
+    },
   },
   config = function(_, opts)
     -- ---@diagnostic disable-next-line: missing-fields
@@ -28,3 +37,26 @@ return {
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
+
+-- ensure_installed = {
+--   'json',
+--   'javascript',
+--   'typescript',
+--   'tsx',
+--   'yaml',
+--   'html',
+--   'css',
+--   'prisma',
+--   'markdown',
+--   'markdown_inline',
+--   'svelte',
+--   'graphql',
+--   'bash',
+--   'lua',
+--   'vim',
+--   'dockerfile',
+--   'gitignore',
+--   'query',
+--   'vimdoc',
+--   'c',
+-- },
