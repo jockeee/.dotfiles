1d7a665ebb34e8846cb654be89143aacfa848e4a
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2024-07-31 17:29:34 +0200

    nvim

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -7,16 +7,16 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>')
 
 -- Exit terminal mode in the builtin terminal with <esc> (default: <C-\><C-n>).
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit Terminal Mode' })
 
 -- Save file
-vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save file' })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
 
 -- Resize window using <ctrl> arrow keys
-vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase window height' })
-vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease window height' })
-vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window hidth' })
-vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window hidth' })
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+vim.keymap.set('n', '<c-down>', '<cmd>resize -2<cr>', { desc = 'decrease Window Height' })
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Height' })
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Height' })
 
 -- Move lines up/down
 vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
@@ -31,10 +31,10 @@ vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
 -- Diagnostic (https://github.com/neovim/nvim-lspconfig)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float, { desc = 'Show diagnostic error messages' })
-vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open diagnostic quickfix list' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous Diagnostic Message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next Diagnostic Message' })
+vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float, { desc = 'Show Diagnostic Error Messages' })
+vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open Diagnostic Quickfix List' })
 
 vim.keymap.set('n', '<leader>nl', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 vim.keymap.set('n', '<leader>nm', '<cmd>Mason<cr>', { desc = 'Mason' })
@@ -43,15 +43,18 @@ vim.keymap.set('n', '<leader>nm', '<cmd>Mason<cr>', { desc = 'Mason' })
 vim.keymap.set('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 vim.keymap.set('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 
--- leader-d:
-vim.keymap.set('n', '<leader>dd', '<cmd>bd<cr>', { desc = 'Buffer delete' })
-vim.keymap.set('n', '<leader>da', '<cmd>%bdelete<cr>', { desc = 'Buffer delete all' })
-vim.keymap.set('n', '<leader>dk', '<cmd>bd!<cr>', { desc = 'Buffer kill (ignore unsaved changes)' })
+-- leader-d: Buffer (document)
+vim.keymap.set('n', '<leader>dd', '<cmd>bd<cr>', { desc = 'Buffer Delete' })
+vim.keymap.set('n', '<leader>da', '<cmd>%bdelete<cr>', { desc = 'Buffer Delete All' })
+vim.keymap.set('n', '<leader>dx', '<cmd>bd!<cr>', { desc = 'Buffer Kill (Ignore Unsaved Changes)' })
 
--- leader-t:
+-- leader-n: Neovim
+vim.keymap.set('n', '<leader>nq', '<cmd>quit<cr>', { desc = 'Neovim Quit' })
+
+-- leader-t: Toggle
 vim.keymap.set(
   'n',
   '<leader>tc',
   '<cmd>lua vim.opt.colorcolumn = vim.inspect(vim.opt.colorcolumn:get()) == "{}" and { 79 } or {}<cr>',
-  { desc = 'Colorcolumn Toggle' }
+  { desc = 'Colorcolumn' }
 )
