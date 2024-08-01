ecc30c27ffa44c37bf9b903f37644fbaf9dc2bf2
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2024-08-01 22:42:56 +0200

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,22 @@
-local wezterm = require("wezterm") -- Pull in the wezterm API
-local config = wezterm.config_builder() -- This will hold the configuration.
+local wezterm = require 'wezterm' -- Pull in the Wezterm API
+local config = wezterm.config_builder() -- This will hold the configuration
 
 config = {
-	automatically_reload_config = true,
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font_size = 16,
-	background = {
-		{
-			source = {
-				Color = "#1f1f1f",
-			},
-			width = "100%",
-			height = "100%",
-		},
-	},
+  automatically_reload_config = true,
+  enable_tab_bar = false,
+  color_scheme = 'Catppuccin Mocha',
+  background = {
+    {
+      source = {
+        Color = '#1f1f1f',
+      },
+      width = '100%',
+      height = '100%',
+    },
+  },
+  font = wezterm.font('MesloLGS Nerd Font', { weight = 'Regular', stretch = 'Normal', style = 'Normal' }),
+  font_size = 16,
+  line_height = 1.05,
 }
 
-return config -- return the configuration to wezterm
+return config -- Return configuration to wezterm
