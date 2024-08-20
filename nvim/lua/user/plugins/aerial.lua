33d7c92e0c4c26a71c9ae9453e8bf8ee9c255b80
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2024-08-20 16:31:56 +0200

    nvim

--- nvim/lua/user/plugins/aerial.lua
+++ nvim/lua/user/plugins/aerial.lua
@@ -2,6 +2,8 @@
 -- https://github.com/stevearc/aerial.nvim
 -- Code outline
 --
+-- You will need to have either Treesitter or a working LSP client.
+-- You can configure your preferred source(s) with the backends option (see Options).
 -- https://github.com/stevearc/aerial.nvim#options
 
 return {
@@ -18,15 +20,15 @@ return {
     },
   },
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
+    'nvim-treesitter/nvim-treesitter', -- optional
+    'nvim-tree/nvim-web-devicons', -- optional
   },
   config = function()
     require('aerial').setup {
       layout = {
         min_width = 20,
       },
-      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      -- use on_attach to set keymaps when aerial has attached to a buffer
       on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
         vim.keymap.set('n', '<leader>{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
