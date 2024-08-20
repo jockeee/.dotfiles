9c0dc20a53694fc7f993893caa51746c4f2fdd1f
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2024-08-20 22:49:45 +0200

    nvim

--- nvim/lua/user/plugins/harpoon.lua
+++ nvim/lua/user/plugins/harpoon.lua
@@ -6,33 +6,38 @@ return {
   event = 'VimEnter',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
+  opts = {
+    settings = {
+      save_on_toggle = true, -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+    },
+  },
+  config = function(_, opts)
     local harpoon = require 'harpoon'
-    harpoon:setup()
+    harpoon:setup(opts)
 
     -- M-h Harpoon List
     -- M-l Add File
     -- M-n Previous
     -- M-m Next
     vim.keymap.set('n', '<M-h>', function()
-      harpoon:list():add()
-    end, { desc = 'Harpoon: List' })
-    vim.keymap.set('n', '<M-l>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Harpoon: Add File' })
+    end)
+    vim.keymap.set('n', '<M-l>', function()
+      harpoon:list():add()
+    end)
     vim.keymap.set('n', '<M-n>', function()
       harpoon:list():prev()
-    end, { desc = 'Harpoon: Previous' })
+    end)
     vim.keymap.set('n', '<M-m>', function()
       harpoon:list():next()
-    end, { desc = 'Harpoon: Next' })
+    end)
 
     -- <alt>a-f for file 1-4
     -- <alt>z-v for file 5-8
     for idx, char in ipairs { 'a', 's', 'd', 'f', 'z', 'x', 'c', 'v' } do
       vim.keymap.set('n', string.format('<M-%s>', char), function()
         harpoon:list():select(idx)
-      end, { desc = string.format('Harpoon: File %d', idx) })
+      end)
     end
   end,
 }
