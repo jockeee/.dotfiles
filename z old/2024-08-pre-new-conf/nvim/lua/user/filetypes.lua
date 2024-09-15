5a48e5a98fed73886aaaf31a1db6ca30390916e7
user <user@local>
2024-09-15 16:06:23 +0200

    removes old

--- z old/2024-08-pre-new-conf/nvim/lua/user/filetypes.lua
+++
@@ -1,6 +0,0 @@
---
--- filetypes.lua
-
--- .templ
--- https://templ.guide/commands-and-tools/ide-support/#neovim--050
-vim.filetype.add { extension = { templ = 'templ' } }
--- z old/2024-08-pre-new-conf/nvim/lua/user/plugins/nvim-autopairs.lua
+++
@@ -1,9 +0,0 @@
---
--- https://github.com/windwp/nvim-autopairs
--- autopairs for neovim written in lua
-
-return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  config = true,
-}
