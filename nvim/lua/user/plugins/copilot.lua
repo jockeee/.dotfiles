4f1f36ec38a3d04e94abb2b6de4f74940027ff3e
user <user@local>
2024-09-24 20:36:22 +0200

    nvim

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -9,8 +9,17 @@ return {
   cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
+    filetypes = {
+      sh = function()
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+          -- disable for .env files
+          return false
+        end
+        return true
+      end,
+      text = false,
+    },
     suggestion = {
-      auto_trigger = true,
       keymap = {
         accept = '<C-a>',
         accept_word = '<C-d>',
