eebc278924f93e8004136b5d9843d37e2772ebfd
user <user@local>
2024-11-16 17:26:30 +0100

    nvim

--- nvim/lua/user/plugins/toggleterm.lua
+++ nvim/lua/user/plugins/toggleterm.lua
@@ -13,12 +13,12 @@ return {
     -- start_in_insert = true,
     -- insert_mappings = true, -- whether or not the open mapping applies in insert mode
     -- terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+    -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
   },
   config = function(_, opts)
     local tt = require 'toggleterm'
     tt.setup(opts)
 
-    -- vim.keymap.set('t', '`', '<C-\\><C-n><cmd>ToggleTerm direction=float<cr>', { desc = 'Exit Terminal' })
     vim.keymap.set('t', '`', '<cmd>ToggleTerm direction=float<cr>', { desc = 'Exit Terminal' })
   end,
 }
