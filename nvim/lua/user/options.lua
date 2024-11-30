304e9dffcfb7b783a28e5113e7252423d8b2b505
user <user@local>
2024-11-30 22:49:48 +0100

    nvim

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -84,5 +84,5 @@ vim.opt.pumheight = 10 -- maximum number of items to show in the popup menu
 
 -- Sessions
 -- auto-session plugin suggests these sessionoptions
--- https://github.com/rmagatti/auto-session?tab=readme-ov-file#recommended-sessionoptions-config
+-- https://github.com/rmagatti/auto-session#recommended-sessionoptions-config
 vim.opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
