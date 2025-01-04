a25ff8ab5d45acf43776a8af5807fe22cbb4dc67
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-01-04 19:47:28 +0100

    nvim

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -60,7 +60,7 @@ vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 
 -- Tabs
-vim.keymap.set({ 'n', 'v', 't' }, '<leader>+', '<cmd>tabnew<cr>', { desc = 'New Tab' })
+vim.keymap.set({ 'n', 'v' }, '<leader>+', '<cmd>tabnew<cr>', { desc = 'New Tab' })
 vim.keymap.set({ 'i', 'n', 'v', 't' }, '<M-v>', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
 vim.keymap.set({ 'i', 'n', 'v', 't' }, '<M-b>', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
 for idx, char in ipairs { 'z', 'x', 'c' } do
