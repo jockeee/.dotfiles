336bbfc709545472b6ca217a81ee3fcd1c497507
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-02-13 21:02:40 +0100

    nvim

--- nvim/lua/user/plugins/completion.lua
+++ nvim/lua/user/plugins/completion.lua
@@ -52,13 +52,13 @@ return {
         ['<C-y>'] = { 'select_and_accept' },
 
         ['<Tab>'] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
+          -- function(cmp)
+          --   if cmp.snippet_active() then
+          --     return cmp.accept()
+          --   else
+          --     return cmp.select_and_accept()
+          --   end
+          -- end,
           'snippet_forward',
           'fallback',
         },
