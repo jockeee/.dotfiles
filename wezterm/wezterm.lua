308b34e812d2c28257236cd0362600290597cd82
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-02-16 16:39:52 +0100

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,7 @@
 
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local mux = wezterm.mux
 
 --
 -- Functions
@@ -45,7 +46,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
     zoomed = ' '
   end
 
-  return '   ' .. zoomed .. title
+  return '  ' .. zoomed .. title
 end)
 
 wezterm.on('update-status', function(window, pane)
@@ -71,6 +72,59 @@ wezterm.on('update-status', function(window, pane)
   window:set_left_status(formatted)
 end)
 
+wezterm.on('gui-startup', function(cmd)
+  -- allow `wezterm start -- something` to affect what we spawn in our initial window
+  local args = {}
+  if cmd then
+    args = cmd.args
+  end
+
+  local home = os.getenv 'HOME'
+
+  local tab, pane, window = mux.spawn_window {
+    workspace = 'home',
+  }
+
+  local tab, pane, window = mux.spawn_window {
+    workspace = '.dot',
+    cwd = home .. '/.dotfiles',
+  }
+
+  local tab, pane, window = mux.spawn_window {
+    workspace = 'nvim',
+    cwd = home .. '/.dotfiles/nvim',
+  }
+
+  local tab, pane, window = mux.spawn_window {
+    workspace = 'code',
+    cwd = home .. '/code',
+  }
+
+  local tab, pane, window = mux.spawn_window {
+    workspace = 'pass',
+    cwd = home .. '/.password-store',
+  }
+
+  -- Set a workspace for coding
+  -- Top pane is for the editor, bottom pane is for the build tool
+  -- local project_dir = home .. '/code'
+  -- local tab, build_pane, window = mux.spawn_window {
+  --   workspace = 'code',
+  --   cwd = project_dir,
+  --   args = args,
+  -- }
+  -- local editor_pane = build_pane:split {
+  --   direction = 'Top',
+  --   size = 0.95,
+  --   cwd = project_dir,
+  -- }
+  -- may as well kick off a build in that pane
+  -- build_pane:send_text 'air\n'
+
+  -- We want to startup in this workspace
+  mux.set_active_workspace 'home'
+end)
+
 --
 -- Config
 --
@@ -81,6 +135,14 @@ if wezterm.config_builder then
   -- http://wezterm.org/config/lua/wezterm/config_builder.html
   config = wezterm.config_builder()
 end
+
+-- This causes `wezterm` to act as though it was started as `wezterm connect unix` by default, connecting to the unix domain on startup.
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
+}
+config.default_gui_startup_args = { 'connect', 'unix' }
 
 config = {
   automatically_reload_config = true,
@@ -120,10 +182,10 @@ config = {
       height = '100%',
     },
   },
-  -- Disable dimming of inactive panes
+  -- Dimming of inactive panes
   inactive_pane_hsb = {
-    saturation = 1.0,
-    brightness = 1.0,
+    saturation = 1.0, -- 1.0 = keep as-is
+    brightness = 1.0, -- 1.0 = keep as-is
   },
   colors = {
     tab_bar = {
@@ -151,63 +213,102 @@ config = {
       },
     },
   },
+}
 
+--
+-- Keymaps
+--
+
+-- disable_default_key_bindings = true,
+
+-- leader
+config.leader = {
+  key = 's',
+  mods = 'CTRL',
+  timeout_milliseconds = 10000, -- default: 1000
+}
+
+config.keys = {
+  -- Send C-s
+  { key = 's', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 's', mods = 'CTRL' } },
+
+  -- Send C-l
+  { key = 'l', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'l', mods = 'CTRL' } },
+
+  -- Workspaces (sessions)
   --
-  -- Keymaps
-
-  -- disable_default_key_bindings = true,
-
-  -- leader
-  leader = {
+  -- Workspaces are used to group related terminal tabs and panes together.
+  -- This is useful for organizing different tasks or projects.
+  -- You can switch between workspaces to focus on different tasks without closing and reopening terminal sessions.
+  --
+  -- Domains represent different environments or contexts in which terminal sessions run.
+  -- They can be local shells, remote SSH sessions, or other types of connections.
+  -- Domains allow you to manage multiple connections and sessions within the same terminal application.
+  {
     key = 's',
-    mods = 'CTRL',
-    timeout_milliseconds = 10000, -- default: 1000
+    mods = 'LEADER',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES', fuzzy_help_text = ': ' },
+  },
+  {
+    key = 'p',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|TABS|LAUNCH_MENU_ITEMS|DOMAINS|KEY_ASSIGNMENTS|WORKSPACES|COMMANDS', fuzzy_help_text = ': ' },
+  },
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = wezterm.action.ShowLauncher,
   },
 
-  keys = {
-    -- Send C-s
-    { key = 's', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 's', mods = 'CTRL' } },
+  { key = 'q', mods = 'META', action = wezterm.action { SwitchToWorkspace = { name = 'home' } } },
+  { key = 'w', mods = 'META', action = wezterm.action { SwitchToWorkspace = { name = '.dot' } } },
+  { key = 'e', mods = 'META', action = wezterm.action { SwitchToWorkspace = { name = 'nvim' } } },
+  { key = 'r', mods = 'META', action = wezterm.action { SwitchToWorkspace = { name = 'code' } } },
+  { key = 't', mods = 'META', action = wezterm.action { SwitchToWorkspace = { name = 'pass' } } },
 
-    -- Workspaces (sessions)
-    { key = 'w', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { flags = 'WORKSPACES' } },
+  -- Tabs (windows)
+  { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivateLastTab },
+  { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
+  { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
 
-    -- Tabs (windows)
-    { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
-    { key = 'l', mods = 'LEADER', action = wezterm.action.ActivateLastTab },
-    { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
-    { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
+  -- Panes (splits)
+  { key = '%', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } }, -- left to right, split along horizontal line
+  { key = '"', mods = 'LEADER|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } }, -- top to bottom, split along vertical line
+  { key = 'z', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
 
-    -- Panes (splits)
-    { key = '%', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } }, -- left to right, split along horizontal line
-    { key = '"', mods = 'LEADER|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } }, -- top to bottom, split along vertical line
-    { key = 'z', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
+  -- Using smart-splits plugin instead
+  -- { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down' },
+  -- { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up' },
+  -- { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left' },
+  -- { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right' },
 
-    -- Using smart-splits plugin for this
-    -- { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down' },
-    -- { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up' },
-    -- { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left' },
-    -- { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right' },
+  -- Copy mode
+  { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+  { key = 'PageUp', mods = 'NONE', action = wezterm.action.ActivateCopyMode },
 
-    -- Copy mode
-    { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
-    {
-      key = 'PageUp',
-      mods = 'NONE',
-      action = wezterm.action.Multiple {
-        wezterm.action.CopyMode 'ClearPattern',
-        wezterm.action.ActivateCopyMode,
-      },
-    },
-
-    -- Quick select
-    { key = 'f', mods = 'LEADER', action = act.QuickSelect },
-    {
-      key = '/',
-      mods = 'LEADER',
-      action = wezterm.action.Search { Regex = '[a-f0-9]{6,}' },
-    },
+  -- Quick select
+  { key = 'f', mods = 'LEADER', action = act.QuickSelect },
+  {
+    key = '/',
+    mods = 'LEADER',
+    action = wezterm.action.Search { Regex = '[a-f0-9]{6,}' },
   },
 }
+
+--
+-- Hyperlinks
+--
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wezterm/wezterm | "wezterm/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+-- table.insert(config.hyperlink_rules, {
+--   regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+--   format = 'https://github.com/$1/$3',
+-- })
 
 --
 -- Plugins
