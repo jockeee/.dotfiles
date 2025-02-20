475cbaa89f3fac3c2de60fb0e9a12ffc4d63edb2
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-02-20 20:15:39 +0100

    nvim

--- nvim/lua/user/plugins/flash.lua
+++ nvim/lua/user/plugins/flash.lua
@@ -4,10 +4,18 @@
 
 return {
   'folke/flash.nvim',
-  event = 'VeryLazy',
+  event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     highlight = {
-      backdrop = false,
+      backdrop = false, -- default: true
+    },
+    modes = {
+      -- options used when flash is activated through
+      -- `f`, `F`, `t`, `T`, `;` and `,` motions
+      char = {
+        enabled = false, -- default: true
+        highlight = { backdrop = false }, -- default: true
+      },
     },
   },
   keys = {
