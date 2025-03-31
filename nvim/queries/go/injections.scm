4940a465d4f38d0c832b90323f7a74fd546bd9db
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-03-31 19:57:36 +0200

    nvim

--- nvim/queries/go/injections.scm
+++ nvim/queries/go/injections.scm
@@ -1,15 +1,15 @@
-; SQL highlighting when we have
-; stmt = `
-;   ... SQL ...`
+; SQL highlighting for:
+;   stmt = `
+;     ... SQL ...`
 left: (expression_list
   (identifier) @var_name (#eq? @var_name "stmt"))
 right: (expression_list
   (raw_string_literal
     (raw_string_literal_content) @injection.content (#set! injection.language "sql")))
 
-; SQL highlighting when we have
-; query = `
-;   ... SQL ...`
+; SQL highlighting for:
+;   query = `
+;     ... SQL ...`
 left: (expression_list
   (identifier) @var_name (#eq? @var_name "query"))
 right: (expression_list
@@ -17,8 +17,8 @@ right: (expression_list
     (raw_string_literal_content) @injection.content (#set! injection.language "sql")))
 
 
-; SQL highlighting when we have
-; stmt := fmt.Sprintf(`
+; SQL highlighting for:
+;   stmt := fmt.Sprintf(`
 ;     ... SQL ...`, filters.sortColumn(), filters.sortDirection())
 ; manually change any %s to valid SQL before formatting, then change back to %s
 left: (expression_list
@@ -32,8 +32,8 @@ right: (expression_list
       (raw_string_literal
     (raw_string_literal_content) @injection.content (#set! injection.language "sql")))))
 
-; SQL highlighting when we have
-; query := fmt.Sprintf(`
+; SQL highlighting for:
+;   query := fmt.Sprintf(`
 ;     ... SQL ...`, filters.sortColumn(), filters.sortDirection())
 ; manually change any %s to valid SQL before formatting, then change back to %s
 left: (expression_list
@@ -46,4 +46,13 @@ right: (expression_list
     arguments: (argument_list
       (raw_string_literal
     (raw_string_literal_content) @injection.content (#set! injection.language "sql")))))
+
+; HTML highlighting for:
+;   html = `
+;     ... HTML ...`
+; manually change any %s to valid SQL before formatting, then change back to %s
+name: (identifier) @var_name (#eq? @var_name "html")
+value: (expression_list
+  (raw_string_literal
+    (raw_string_literal_content) @injection.content (#set! injection.language "html")))
 
