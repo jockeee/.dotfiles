47bd56ec5e7ae9fdfc3361b61aa80e7a76202251
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-06-03 16:22:51 +0200

    …

--- nvim/lua/user/plugins/copilot.lua
+++ nvim/lua/user/plugins/copilot.lua
@@ -14,7 +14,7 @@ return {
       -- Disable Copilot if filetype is empty
       if vim.bo.filetype == '' then return false end
       -- Disable Copilot for certain file patterns
-      local disabled_patterns = { '^%.env.*', '%.key$', '%.fullchain$' }
+      local disabled_patterns = { '^%.env.*', '^*.%.key$', '^*.%.fullchain$' }
       local filename = vim.fs.basename(vim.api.nvim_buf_get_name(0))
       for _, pattern in ipairs(disabled_patterns) do
         if string.match(filename, pattern) then return false end
