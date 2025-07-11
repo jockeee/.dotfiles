c62ad08124a0231af79db80fe31a8dbc497648cc
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-07-11 22:54:53 +0200

    …

--- symlink.sh
+++ symlink.sh
@@ -7,147 +7,132 @@ fi
 
 # bash
 if [ -d ~/.config/bash ]; then
-    echo "[skip]: ~/.config/bash"
+    echo -e "\e[33m[skip]: ~/.config/bash\e[0m"
 else
-    echo -n "[info]: ~/.config/bash... "
+    echo -e "\e[32m[info]: ~/.config/bash"
     mkdir -p ~/.config/bash
     ln -s ~/.dotfiles/bash/bashrc ~/.config/bash/.
     ln -s ~/.dotfiles/bash/bash_aliases ~/.config/bash/.
-    echo "OK"
 fi
+# ~/.bashrc
 if [ -f ~/.bashrc ] && grep -q "# default distro ~/.bashrc above" ~/.bashrc; then
-    echo "[skip]: ~/.bashrc"
+    echo -e "\e[33m[skip]: ~/.bashrc\e[0m"
 else
-    echo -n "[info]: ~/.bashrc... "
+    echo -e "\e[32m[info]: ~/.bashrc \e[0m"
     cat >>~/.bashrc <<EOF
 
 # default distro ~/.bashrc above
-
 export XDG_CONFIG_HOME="\${XDG_CONFIG_HOME:-\$HOME/.config}"
-
 if [ -f "\$XDG_CONFIG_HOME/bash/bashrc" ]; then
     source "\$XDG_CONFIG_HOME/bash/bashrc"
 fi
 EOF
-    echo "OK"
 fi
 
 # bat
 if [ -d ~/.config/bat ]; then
-    echo "[skip]: ~/.config/bat"
+    echo -e "\e[33m[skip]: ~/.config/bat\e[0m"
 else
-    echo -n "[info]: ~/.config/bat... "
+    echo -e "\e[32m[info]: ~/.config/bat\e[0m"
     ln -s ~/.dotfiles/bat ~/.config/.
     if command -v bat &>/dev/null; then
         /usr/bin/bat cache --build
     fi
-    echo "OK"
 fi
 
 # eza
 if [ -d ~/.config/eza ]; then
-    echo "[skip]: ~/.config/eza"
+    echo -e "\e[33m[skip]: ~/.config/eza\e[0m"
 else
-    echo -n "[info]: ~/.config/eza... "
+    echo -e "\e[32m[info]: ~/.config/eza\e[0m"
     ln -s ~/.dotfiles/eza ~/.config/.
-    echo "OK"
 fi
 
 # fish
 if [ -d ~/.config/fish ]; then
-    echo "[skip]: ~/.config/fish"
+    echo -e "\e[33m[skip]: ~/.config/fish\e[0m"
 else
-    echo -n "[info]: ~/.config/fish... "
+    echo -e "\e[32m[info]: ~/.config/fish\e[0m"
     mkdir -p ~/.config/fish
     ln -s ~/.dotfiles/fish/alias.fish ~/.config/fish/.
     ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/.
-    echo "OK"
 fi
 
 # hidden
 if [ -f ~/.hidden ]; then
-    echo "[skip]: ~/.hidden"
+    echo -e "\e[33m[skip]: ~/.hidden\e[0m"
 else
-    echo -n "[info]: ~/.hidden... "
+    echo -e "\e[32m[info]: ~/.hidden\e[0m"
     ln -s ~/.dotfiles/.hidden ~/.
-    echo "OK"
 fi
 
 # lazygit
 if [ -d ~/.config/lazygit ]; then
-    echo "[skip]: ~/.config/lazygit"
+    echo -e "\e[33m[skip]: ~/.config/lazygit\e[0m"
 else
-    echo -n "[info]: ~/.config/lazygit... "
+    echo -e "\e[32m[info]: ~/.config/lazygit\e[0m"
     ln -s ~/.dotfiles/lazygit ~/.config/.
-    echo "OK"
 fi
 
 # nvim
 if [ -d ~/.config/nvim ]; then
-    echo "[skip]: ~/.config/nvim"
+    echo -e "\e[33m[skip]: ~/.config/nvim\e[0m"
 else
-    echo -n "[info]: ~/.config/nvim... "
+    echo -e "\e[32m[info]: ~/.config/nvim\e[0m"
     ln -s ~/.dotfiles/nvim ~/.config/.
-    echo "OK"
 fi
 
 # tmux
 # if [ -d ~/.config/tmux ]; then
-#     echo "[skip]: ~/.config/tmux"
+#     echo -e "\e[33m[skip]: ~/.config/tmux\e[0m"
 # else
-#     echo -n "[info]: ~/.config/tmux... "
+#     echo -e "\e[32m[info]: ~/.config/tmux\e[0m"
 #     mkdir -p ~/.config/tmux
 #     ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/.
 #
 #     if command -v git &>/dev/null; then
 #         git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 #     fi
-#     echo "OK"
 # fi
 
 # tmux-sessionizer
 # if [ -f ~/.local/bin/tmux-sessionizer.sh ]; then
-#     echo "[skip]: ~/.local/bin/tmux-sessionizer.sh"
+#     echo -e "\e[33m[skip]: ~/.local/bin/tmux-sessionizer.sh\e[0m"
 # else
-#     echo -n "[info]: ~/.local/bin/tmux-sessionizer.sh... "
+#     echo -e "\e[32m[info]: ~/.local/bin/tmux-sessionizer.sh\e[0m"
 #     ln -s ~/.dotfiles/bin/tmux-sessionizer.sh ~/.local/bin/.
-#     echo "OK"
 # fi
 
 # wezterm
 if [ -d ~/.config/wezterm ]; then
-    echo "[skip]: ~/.config/wezterm"
+    echo -e "\e[33m[skip]: ~/.config/wezterm\e[0m"
 else
-    echo -n "[info]: ~/.config/wezterm... "
+    echo -e "\e[32m[info]: ~/.config/wezterm\e[0m"
     ln -s ~/.dotfiles/wezterm ~/.config/.
-    echo "OK"
 fi
 
-# zsh
+# ~/.config/zsh
 if [ -d ~/.config/zsh ]; then
-    echo "[skip]: ~/.config/zsh"
+    echo -e "\e[33m[skip]: ~/.config/zsh\e[0m"
 else
-    echo -n "[info]: ~/.config/zsh... "
+    echo -e "\e[32m[info]: ~/.config/zsh\e[0m"
     mkdir -p ~/.config/zsh
-    ln -s ~/.dotfiles/zsh/zsh ~/.config/zsh/.
-    echo "OK"
+    ln -s ~/.dotfiles/bash/bashrc ~/.config/zsh/zshrc
 fi
 
+# ~/.zshrc
 if [ -f ~/.zshrc ] && grep -q "# default distro ~/.zshrc above" ~/.zshrc; then
-    echo "[skip]: ~/.zshrc"
+    echo -e "\e[33m[skip]: ~/.zshrc\e[0m"
 else
-    echo -n "[info]: ~/.zshrc... "
+    echo -e "\e[32m[info]: ~/.zshrc\e[0m"
     cat >>~/.zshrc <<EOF
 
 # default distro ~/.zshrc above
-
 export XDG_CONFIG_HOME="\${XDG_CONFIG_HOME:-\$HOME/.config}"
-
 if [ -f "\$XDG_CONFIG_HOME/zsh/zshrc" ]; then
     source "\$XDG_CONFIG_HOME/zsh/zshrc"
 fi
 EOF
-    echo "OK"
 fi
 
 # #!/usr/bin/env bash
