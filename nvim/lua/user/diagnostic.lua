8e7d1ecdd4ab22bad990c34c81c8fb0b074f9548
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-07-12 21:01:56 +0200

    Refactor diagnostic configuration and keymap setup in Neovim configuration

--- nvim/lua/user/diagnostic.lua
+++ nvim/lua/user/diagnostic.lua
@@ -15,84 +15,28 @@ vim.diagnostic.config {
       [vim.diagnostic.severity.HINT] = '󰌶 ',
     },
   },
-  -- virtual_lines = true, -- show all in buffer
+  -- virtual_lines = false, -- show all in buffer
   -- virtual_lines = {
   --   current_line = true, -- show only on current line
   -- },
   -- virtual_text = true,
-  -- virtual_text = {
-  --   enabled = false,
-  --   spacing = 8,
-  --   source = 'if_many',
-  -- },
 }
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Diagnostic: show message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Diagnostics: show message' })
 
--- virtual_lines, off
-vim.keymap.set('n', '<leader>sDo', function()
-  vim.diagnostic.config {
-    virtual_lines = false,
-  }
-end, { desc = 'off' })
+vim.keymap.set('n', '<leader>sd', function()
+  local config = vim.diagnostic.config()
+  local vl = config and config.virtual_lines
 
--- virtual_lines, buffer
-vim.keymap.set('n', '<leader>sDb', function()
-  vim.diagnostic.config {
-    virtual_lines = true,
-  }
-end, { desc = 'buffer' })
+  local enabled = false
+  if type(vl) == 'table' then
+    enabled = vl.current_line or false
+  elseif type(vl) == 'boolean' then
+    enabled = vl
+  end
 
--- virtual_lines, current line
-vim.keymap.set('n', '<leader>sDl', function()
-  vim.diagnostic.config {
-    virtual_lines = {
-      current_line = true,
-    },
-  }
-end, { desc = 'current line' })
-
--- virtual_text, off
-vim.keymap.set('n', '<leader>sdo', function()
-  vim.diagnostic.config {
-    virtual_text = false,
-  }
-end, { desc = 'off' })
-
--- virtual_text, buffer
-vim.keymap.set('n', '<leader>sdb', function()
-  vim.diagnostic.config {
-    virtual_text = {
-      current_line = false,
-      spacing = 8,
-      source = 'if_many',
-    },
-  }
-end, { desc = 'buffer' })
-
-vim.keymap.set('n', '<leader>sdl', function()
-  vim.diagnostic.config {
-    virtual_text = {
-      current_line = true,
-      spacing = 8,
-      source = 'if_many',
-    },
-  }
-end, { desc = 'current line' })
-
--- vim.keymap.set('n', '<leader>sv', function()
---   local config = vim.diagnostic.config()
---   local current = config and config.virtual_lines
---
---   local enabled = false
---   if type(current) == 'table' then
---     enabled = current.current_line or false
---   elseif type(current) == 'boolean' then
---     enabled = current
---   end
---
---   vim.diagnostic.config { virtual_lines = { current_line = not enabled } }
--- end, { desc = 'Diagnostic: virtual lines' })
+  vim.diagnostic.config { virtual_lines = not enabled }
+end, { desc = 'Diagnostics: virtual lines' })
 
 -- Diagnostics (https://github.com/neovim/nvim-lspconfig)
 -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Prev Diagnostic Message' }) -- default in v0.11, [d
