eac4a2d480da1665096b20409583e9a5a8f4ef82
Johan Petersson <5463134+jockeee@users.noreply.github.com>
2025-08-30 23:13:32 +0200

    Enable guess-indent plugin; set PHP indent to 2 spaces

--- nvim/lua/user/filetypes.lua
+++ nvim/lua/user/filetypes.lua
@@ -105,8 +105,8 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'php',
   callback = function()
     vim.opt_local.expandtab = true
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.tabstop = 2
   end,
 })
